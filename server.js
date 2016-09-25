10bc84f8d332d720d3627efe0eae9f8e97545d70
ujjaldas1997 <ujjaldas223@gmail.com>
2016-09-26 00:00:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'IMG_20160910_220653.jpg'));
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
