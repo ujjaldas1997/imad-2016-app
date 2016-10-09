376702907d9174699a9c8788b2ddb7076a6e5fb4
ujjaldas1997 <ujjaldas223@gmail.com>
2016-10-09 12:12:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,7 +46,7 @@ submit.onclick = function(){
     
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET' , 'http:/ujjaldas1997.imad.hasura-app.io/submit?name=' + name , true);
+    request.open('GET' , 'http://ujjaldas1997.imad.hasura-app.io/submit?name=' + name , true);
     request.send(null);
     
 };
