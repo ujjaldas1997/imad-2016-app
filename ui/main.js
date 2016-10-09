86e60ce0914a99382056704a26779042d341c82b
ujjaldas1997 <ujjaldas223@gmail.com>
2016-10-09 12:05:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,6 @@ button.onclick = function(){
     
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function(){
@@ -46,7 +44,8 @@ submit.onclick = function(){
     
     };
     
-    
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET' , 'http:/ujjaldas1997.imad.hasura-app.io/submit?name=' + name , true);
     request.send(null);
     
