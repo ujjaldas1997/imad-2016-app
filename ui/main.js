a442e223024f8f7d6201f0fe9df4627c76deb8f3
ujjaldas1997 <ujjaldas223@gmail.com>
2016-10-09 10:52:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ submit.onclick = function(){
     
     var names = ['name1' , 'name2' , 'name3' , 'name4' ];    
     var list = '';
-    for(var i=0; i<names.lenght; i++){
+    for(var i=0; i<names.length; i++){
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
