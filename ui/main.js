67506850f4cbf4f989c07dc4f4af29ebf8a3ae7a
ujjaldas1997 <ujjaldas223@gmail.com>
2016-10-09 11:38:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,11 +25,29 @@ var submit = document.getElementById('submit_btn');
 
 submit.onclick = function(){
     
-    var names = ['name1' , 'name2' , 'name3' , 'name4' ];    
-    var list = '';
-    for(var i=0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>';
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        
+    if(request.readyState === XMLHttpRequest.DONE){
+        
+        if(request.status === 200){
+            var names = request.ResponseText;
+            names = JSON.parse(names);
+            var list = '';
+            for(var i=0; i<names.length; i++){
+                list += '<li>' + names[i] + '</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list ;
+        }
+        
     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list ;
+    
+    };
+    
+    
+    request.open('GET' , 'http:/ujjaldas1997.imad.hasura-app.io/submit?name=' + name , true);
+    request.send(null);
+    
 };
